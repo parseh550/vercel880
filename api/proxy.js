d3618225984833a2fce23511dd357592b8e9aa80
parseh550 <parseh550@gmail.com>
2025-06-27 11:03:11 +0330

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,13 +1,21 @@
+const https = require('https');
 const fetch = require('node-fetch');
 
+const agent = new https.Agent({
+  rejectUnauthorized: false,  // فقط برای تست؛ در محیط واقعی استفاده نکنید
+  secureProtocol: 'TLS_method' // می‌توانید 'TLSv1_2_method' یا 'TLSv1_3_method' امتحان کنید
+});
+
 module.exports = async (req, res) => {
-  const url = new URL(req.url, 'https://jal.id.ir').toString();
+  const baseUrl = 'https://jal.id.ir';
+  const url = new URL(req.url, baseUrl).toString();
 
   try {
     const response = await fetch(url, {
       method: req.method,
       headers: req.headers,
       body: ['GET', 'HEAD'].includes(req.method) ? undefined : req,
+      agent: agent,
     });
 
     res.writeHead(response.status, Object.fromEntries(response.headers));
